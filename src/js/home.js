2bfde770201f0f9305378505f359b13d56aa4699
KaterynaBachkalo <kozlovets88@gmail.com>
2023-09-01 00:30:07 +0200

    create  getAllCategoriesBookTopList

---
+++ src/js/home.js
@@ -0,0 +1,47 @@
+import Book_api from './APIs/book-api';
+
+let categoryDivWraper = '';
+
+// book.getBookByCategory('Picture Books').then(console.log);
+
+// book.getBookById('643282b1e85766588626a0dc').then(console.log);
+getAllCategoriesBookTopList();
+
+export function getAllCategoriesBookTopList() {
+  const book = new Book_api();
+
+  categoryDivWraper = document.querySelector('.category-wraper');
+
+  book.getTopBooks().then(resp => {
+    renderMarkup(resp);
+  });
+}
+
+function renderMarkup(resp) {
+  categoryDivWraper.insertAdjacentHTML(
+    'beforeend',
+    `<h1 class = "">Best Sellers <span class = "">Books</span></h1>`
+  );
+
+  resp
+    .map(({ list_name, books }) =>
+      renderCategoriesBookTopList(list_name, books)
+    )
+    .join('');
+}
+
+function renderCategoriesBookTopList(list_name, books) {
+  let listLi = '';
+  books.map(({ book_image, title, author }) => {
+    const markup = `<li class = "">
+              <a href="#" class="link"><img class="img" src="${book_image}">
+              <h3 class = "">${title}</h3>
+              <p class = "">${author}</p>
+              </a></li>`;
+    listLi += markup;
+  });
+
+  const categoryDiv = `<div class = ""><h2 class = "">${list_name}</h2>
+          <ul class="category-list">${listLi}</ul></div>`;
+  categoryDivWraper.insertAdjacentHTML('beforeend', categoryDiv);
+}
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import './js/header'
-import './js/home'
-import './js/popup'
-import './js/sidebar'
-import './js/start'
+import './js/header';
+import './js/home';
+import './js/popup';
+import './js/sidebar';
+import './js/start';
 
-// import Book_api from "./js/APIs/book-api";
+// import Book_api from './js/APIs/book-api';
 
 // const book = new Book_api();
 
-// book.getBookByCategory('Picture Books').then(console.log)
-// book.getCategoryList().then(console.log)
-// book.getTopBooks().then(console.log)
+// book.getBookByCategory('Picture Books').then(console.log);
+// book.getCategoryList().then(console.log);
+// book.getTopBooks().then(console.log);
 // book.getBookById('643282b1e85766588626a0dc').then(console.log);
