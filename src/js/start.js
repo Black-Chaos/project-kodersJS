b3060fd610d544ce8da8bc32178b9b6f53faf5ec
Lashtabeg <tahcik@live.com>
2023-09-03 20:57:19 +0800

    feat: form-js (save input value to Local-storage

---
+++ src/js/start.js
@@ -0,0 +1,28 @@
+const refs = {
+  startBackdrop: document.querySelector('.start-backdrop'),
+  startCloseBtn: document.querySelector('.start-close-btn'),
+  form: document.querySelector('.start-form'),
+};
+
+const LOCAL_KEY = 'form-data';
+let inputData = {};
+
+refs.startCloseBtn.addEventListener('click', onCloseBtn);
+refs.form.addEventListener('input', onFormInput);
+refs.form.addEventListener('submit', onFormSub);
+
+function onCloseBtn() {
+  refs.startBackdrop.classList.add('is-hidden');
+}
+
+function onFormSub(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+}
+
+function onFormInput(eve) {
+  const { name, value } = eve.target;
+  inputData[name] = value.trim();
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(inputData));
+  console.log(inputData);
+}
