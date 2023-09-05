c429c4e59a3ae88813f9c996fab57d83641fceaa
Black Chaos <black.chaos.no1@gmail.com>
2023-09-05 23:40:58 +0300

    refactor: refactor categories.js

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,14 +1,8 @@
-// import { getCategoryList } from './APIs/book-api';
 import Book_api from './APIs/book-api';
-import { getAllCategoriesBookTopList } from './home';
-import { getBooksOfCategory } from './home';
+import { getAllCategoriesBookTopList, getBooksOfCategory } from './home';
 
 const categoryColumn = document.querySelector('.categories-elements');
-// const list = document.querySelector('.categories-elements');
-
-// this is for the active class
-let links = document.querySelectorAll('.category-link');
-const categoryDivWraper = document.querySelector('.category-wrapper');
+const categoryDivWrapper = document.querySelector('.category-wrapper');
 
 const book = new Book_api();
 
@@ -25,67 +19,31 @@ function markupColumn() {
         .sort()
         .join('');
 
-      console.log(data);
       categoryColumn.insertAdjacentHTML('beforeend', markupCategoriesColumn);
-      links = document.querySelectorAll('.category-link');
-      console.log(links);
-      clearAll(links);
-      showLink(links);
     })
     .catch(error => console.log(error));
 }
-console.log(links);
-
-// Active class
-function clearAll(array) {
-  array.forEach(item => {
-    item.classList.remove('active');
-  });
-}
-function showLink(array, i = 0) {
-  array[i].classList.add('active');
-}
 
 categoryColumn.addEventListener('click', onCategoriesHandle);
-async function onCategoriesHandle(event) {
+function onCategoriesHandle(event) {
   event.preventDefault();
-  // const target = event.target;
-
-  if (event.target && event.target.classList.contains('category-link')) {
-    links.forEach((item, i) => {
-      if (event.target == item) {
-        clearAll(links);
-        showLink(links, i);
-      }
-    });
-  }
-  console.log(event.target.textContent);
 
   if (event.target.nodeName !== 'A') {
     return;
   }
+  if (event.target.parentNode.classList.contains('active')) return;
 
-  try {
-    // const data = await book.getBookByCategory(event.target.textContent);
-    // console.log(data);
-    const markup = getBooksOfCategory(event.target.textContent);
-
-    clearMarkup();
-    categoryDivWraper.insertAdjacentHTML('beforeend', markup);
-  } catch (error) {
-    console.log(error);
-  }
+  categoryColumn.querySelector('.active').classList.remove('active');
+  event.target.classList.add('active');
 
   if (event.target.textContent === 'All categories') {
-    try {
-      const markup = getAllCategoriesBookTopList(event.target.textContent);
-
-      clearMarkup();
-      categoryDivWraper.insertAdjacentHTML('beforeend', markup);
-    } catch (error) {
-      console.log(error);
-    }
+    clearMarkup();
+    getAllCategoriesBookTopList();
+    return;
   }
+
+  clearMarkup();
+  getBooksOfCategory(event.target.textContent);
 }
 
-const clearMarkup = () => (categoryDivWraper.innerHTML = '');
+const clearMarkup = () => (categoryDivWrapper.innerHTML = '');
